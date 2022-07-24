7301ca0469fef47dedbe95f9e68a6f9fbef14d06
Mrityunjay Sarkar <mrityunjaysorcar@gmail.com>
2022-07-24 10:31:32 +0530

    done

--- signUp/signupscripts/activity.js
+++
@@ -1,9 +0,0 @@
-let show=(e)=>{
-    e.preventDefault()
-    window.location.href="/signuppage/plan.html"
-}
-document.querySelector("#btn").addEventListener("click",show);
-function showBorder(){
-    document.querySelector("#show>div").style= "border:3px solid #1bc98e";
-   document.querySelector("#show>div").style= "color:#1bc98e";
-}
--- signUp/signupscripts/firstpage.js
+++
@@ -1,9 +0,0 @@
-
-let nextPage= (e)=>{
-    e.preventDefault();
-    window.location.href="/signuppage/choose.html"
-}
-document.querySelector("#btn").addEventListener("click",nextPage);
-let goBack=()=>{
-    window.location.href="login.html";
-}
